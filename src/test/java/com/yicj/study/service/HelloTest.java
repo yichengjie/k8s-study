0d91d3bca99cfacfb4c053911cd0d1254f2dd93b
易成杰 <626659321@qq.com>
2022-05-13 08:42:01 +0800

    补充hello world

---
+++ src/test/java/com/yicj/study/service/HelloTest.java
@@ -0,0 +1,9 @@
+package com.yicj.study.service;
+
+import com.yicj.study.K8sApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = K8sApplication.class)
+public class HelloTest {
+
+}
