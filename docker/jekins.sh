16f4a6aab14ba0aca91cd7e354cb5651bf627270
易成杰 <626659321@qq.com>
2022-05-14 22:03:26 +0800

    保存

--- docker/jekins.sh
+++ docker/jekins.sh
@@ -3,7 +3,7 @@ IMAGE=$(docker build . | tail -l | awk '{ print $NF }')
 # 构建挂在到Docker的目录
 MNT="$WORKSPACE/.."
 # 在docker 里执行编译测试
-CONTAINER=$(docker run -d -v "$MNT:/opt/project" $IMAGE /bin/bash -c 'cd /opt/project/workspace && rake spec')
+CONTAINER=$(sudo docker run -d -v $MNT:/opt/project/ $IMAGE /bin/sh -c 'cd /opt/project/workspace && rake spec')
 # 进入容器，这样可以看到输出的内容
 docker attatch $CONTAINER
 # 等待程序退出，得到返回码
