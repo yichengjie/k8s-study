4dcddbee4872c925d2b3373639c8a33060d58c84
易成杰 <626659321@qq.com>
2022-05-14 20:26:14 +0800

    删除多余行

--- docker/jekins.sh
+++ docker/jekins.sh
@@ -4,7 +4,6 @@ IMAGE=$(docker build . | tail -l | awk '{ print $NF }')
 MNT="$WORKSPACE/.."
 # 在docker 里执行编译测试
 CONTAINER=$(docker run -d -v "$MNT:/opt/project" $IMAGE /bin/bash -c 'cd /opt/project/workspace && rake spec')
-CONTAINER=$(docker run -d -v $MNT:/opt/project/ $IMAGE /bin/bash -c 'cd /opt/project/workspace && rake spec')
 # 进入容器，这样可以看到输出的内容
 docker attatch $CONTAINER
 # 等待程序退出，得到返回码
